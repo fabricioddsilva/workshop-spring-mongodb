b88d90b7619c6f3b7aed3cb48aa10d49535e01d2
Fabricio Dias <fabriciodsilva3404@gmail.com>
2024-08-13 15:15:30 -0300

    Consulta simples com Query Methods

--- src/main/java/com/kaos/workshopmongo/config/Instantiation.java
+++ src/main/java/com/kaos/workshopmongo/config/Instantiation.java
@@ -63,7 +63,7 @@ public class Instantiation implements CommandLineRunner {
 
         post1.getComments().addAll(Arrays.asList(c1, c2));
         post2.getComments().add(c2);
-        
+
         postRepository.saveAll(Arrays.asList(post1, post2));
 
         maria.getPosts().addAll(Arrays.asList(post1, post2));
