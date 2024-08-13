166d1d8696fc68aba9b483f173d4ca35c2444517
Fabricio Dias <fabriciodsilva3404@gmail.com>
2024-08-13 12:08:08 -0300

    Inserção de User

--- src/main/java/com/kaos/workshopmongo/services/UserService.java
+++ src/main/java/com/kaos/workshopmongo/services/UserService.java
@@ -1,6 +1,7 @@
 package com.kaos.workshopmongo.services;
 
 import com.kaos.workshopmongo.entities.User;
+import com.kaos.workshopmongo.entities.dto.UserDTO;
 import com.kaos.workshopmongo.repositories.UserRepository;
 import com.kaos.workshopmongo.services.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -25,6 +26,14 @@ public class UserService {
             throw new ObjectNotFoundException("Objeto não encontrado");
         }
         return user.get();
+    }
+
+    public User insert(User obj){
+        return repository.insert(obj);
+    }
+
+    public User fromDTO (UserDTO userDTO){
+        return new User(userDTO.getId(), userDTO.getName(), userDTO.getEmail());
     }
 
 }
