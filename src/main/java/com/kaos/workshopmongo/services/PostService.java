4233231d6084510068f7ae7bd2d063d4b00a0087
Fabricio Dias <fabriciodsilva3404@gmail.com>
2024-08-13 14:49:08 -0300

    Obtendo um post por id

---
+++ src/main/java/com/kaos/workshopmongo/services/PostService.java
@@ -0,0 +1,24 @@
+package com.kaos.workshopmongo.services;
+
+import com.kaos.workshopmongo.entities.Post;
+import com.kaos.workshopmongo.repositories.PostRepository;
+import com.kaos.workshopmongo.services.exceptions.ObjectNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class PostService {
+
+    @Autowired
+    private PostRepository repository;
+
+    public Post findById(String id){
+        Optional<Post> post = repository.findById(id);
+        if (post.isEmpty()){
+            throw new ObjectNotFoundException(id);
+        }
+        return post.get();
+    }
+}
